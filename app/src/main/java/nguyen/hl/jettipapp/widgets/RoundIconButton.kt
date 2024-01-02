dca872ec37e8a07423edc9abbd4a9e92b85c084c
nguyenzye247 <lenguyen2470@gmail.com>
2024-01-02 21:13:00 +0700

    feat: jet tip app using compose

---
+++ app/src/main/java/nguyen/hl/jettipapp/widgets/RoundIconButton.kt
@@ -0,0 +1,60 @@
+package nguyen.hl.jettipapp.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import nguyen.hl.jettipapp.ui.theme.JetTipAppTheme
+
+val IconButtonSizeModifier = Modifier.size(40.dp)
+
+@Composable
+fun RoundIconButton(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector = Icons.Default.Add,
+    onClick: () -> Unit = {},
+    iconTint: Color = Color.Black.copy(alpha = 0.8f),
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
+    elevation: Dp = 4.dp
+) {
+    Card(
+        modifier = modifier
+            .padding(4.dp)
+            .clickable { onClick.invoke() }
+            .then(IconButtonSizeModifier),
+        shape = CircleShape,
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+        elevation = CardDefaults.cardElevation(defaultElevation = elevation)
+    ) {
+        Icon(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(3.dp),
+            imageVector = imageVector,
+            contentDescription = "Plus or minus icon",
+            tint = iconTint
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewRoundIconButton() {
+    JetTipAppTheme {
+        RoundIconButton()
+    }
+}
